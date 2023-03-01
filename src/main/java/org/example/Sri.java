eaa92dc972aadf73b25a1e91c4eeffa19fea5179
srini <srinivasan.k@tringapps.com>
2023-03-01 15:47:58 +0530

    2nd

--- src/main/java/org/example/Sri.java
+++ src/main/java/org/example/Sri.java
@@ -1,5 +1,4 @@
 package org.example;
-
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -28,21 +27,3 @@ class Sri{
         }
     }
 }
-class Card implements Cloneable{
-    String name;
-    String number ;
-    String date;
-    Card(String name,String number,String date){
-        this.name =name;
-        this.number =number;
-        this.date =date;
-    }
-    public Object clone() throws CloneNotSupportedException
-    {
-        return super.clone();
-    }
-    public boolean equalling(String number1){
-        return number.equals(number1);
-    }
-}
-
